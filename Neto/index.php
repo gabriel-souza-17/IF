00246bee2667e17d05687d9230180ef39aabf4b6
Gabriel <aifgoiano@gmail.com>
2026-03-03 20:31:50 -0300

    Adicionando Pastas

---
+++ Neto/index.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Servidor do Matheus
+        
+    </title>
+</head>
+<body>
+    <?php
+echo "Docker PHP funcionando 🚀";
+?>
+</body>
+</html>
