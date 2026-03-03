273afda7e5ec99666daf1ed4a346b39bda766323
Gabriel <aifgoiano@gmail.com>
2026-03-03 20:24:07 -0300

    Adicionando minha pasta pessoal

--- Gabriel/index.php
+++ Gabriel/index.php
@@ -1,9 +1,8 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Servidor</title>
+    <title>Servidor do Gabriel</title>
 </head>
 <body>
     <?php
