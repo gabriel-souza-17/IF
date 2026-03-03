f703f848d2a8b27c1039d2c12ab52501e0354dac
TheusZ21 <victormatheusribeiro2014@outlook.com>
2026-03-03 20:41:05 -0300

    Update index.php

--- Victor Matheus/index.php
+++ Victor Matheus/index.php
@@ -1,14 +1,33 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Servidor do Matheus
-        
-    </title>
+    <title>Document</title>
 </head>
 <body>
-    <?php
-echo "Docker PHP funcionando 🚀";
-?>
+    <h1>Calculo de IMC </h1>
+    <form action="" method="post">
+        <label for="peso">peso</label><br>
+        <input type="number" name="peso" id="peso" required><br>
+        <label for="altura">altura</label><br>
+        <input type="number" name="altura" id="altura" required>
+        <input type="submit" name="calcular" value="calcular">
+
+        <?php 
+            $imc = floatval($peso) / (floatval($altura) * floatval($altura));
+
+            if (isset($_REQUEST['calcular'])) {
+            if ($imc < 18.5) {
+                echo "O seu IMC é: {$imc}, você está abaixo do peso";
+            } else if ($imc < 25) {
+                echo "O seu IMC é: {$imc}, você está com o peso ideal";
+            } else if ($imc < 30) {
+                echo "O seu IMC é: {$imc}, você está acima do peso";
+            } else {
+                echo "O seu IMC é: {$imc}, você está obeso";
+            }
+            }
+        ?>
 </body>
 </html>
