dfae6d34522c2601f44f514a9b3dd0add1fd26fc
Gabriel <aifgoiano@gmail.com>
2026-03-03 20:35:21 -0300

    Adicionando Pastas

--- Neto/index.php
+++ Neto/index.php
@@ -4,9 +4,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Servidor do Neto</title>
 </head>
-        
-    </title>
-</head>
 <body>
     <?php
 echo "Docker PHP funcionando 🚀";
