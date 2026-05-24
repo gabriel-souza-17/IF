6d2339197b90fc95acc59b66a9b4b58fceaef4ec
Gabriel <aifgoiano@gmail.com>
2026-05-24 13:10:09 -0300

    Atualizando tela e conexão da tela de barbeiro

--- www/Views/User/Dashboard/index.php
+++ www/Views/User/Dashboard/index.php
@@ -1,30 +1,110 @@
 <?php
 
-require 'sidebar.php';
-require 'topbar.php';
+ob_start();
 
 ?>
 
-<div class="content">
+<div class="sidebar">
 
-<?php require 'cards.php'; ?>
+<div class="logo">
 
-<div class="row">
-
-<div class="col-lg-8">
-
-<?php require 'agenda.php'; ?>
+✂ Easy Barber
 
 </div>
 
-<div class="col-lg-4">
+<div class="menu">
 
-<?php require 'next_client.php'; ?>
+<a href="">Dashboard</a>
 
-<?php require 'quick_actions.php'; ?>
+<a href="">Agenda</a>
+
+<a href="">Minha Equipe</a>
+
+<a href="">Serviços</a>
+
+<a href="">Visagismo IA</a>
+
+<a href="">Configurações</a>
 
 </div>
 
 </div>
 
+<div class="main">
+
+<h1>
+
+Olá,
+<?= $_SESSION['usuario_nome']; ?>
+👋
+
+</h1>
+
+<p class="subtitle">
+
+Sua agenda está pronta
+
+</p>
+
+<div class="dashboard-box">
+
+<div class="top-row">
+
+<h2>
+
+Próximos Serviços
+
+</h2>
+
+<button class="btn-gold">
+
+Adicionar Serviço
+
+</button>
+
 </div>
+
+<?php
+
+for(
+$i=0;
+$i<3;
+$i++
+):
+
+?>
+
+<div class="card-agenda">
+
+<strong>
+
+Sem agendamento
+
+</strong>
+
+<br>
+
+Cliente aguardando
+
+<br>
+
+Nenhum serviço
+
+</div>
+
+<?php endfor; ?>
+
+</div>
+
+</div>
+
+<?php
+
+$content =
+ob_get_clean();
+
+require
+__DIR__ .
+'/../../../Templates/barbeiro.php';
+
+?>
