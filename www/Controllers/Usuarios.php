4b0cf4ad477cb6f5443270251d067f8f41a1c5ae
Gabriel <aifgoiano@gmail.com>
2026-06-01 19:37:58 -0300

    Adicionando mudanças

--- www/Controllers/Usuarios.php
+++ www/Controllers/Usuarios.php
@@ -1,53 +1,128 @@
 <?php
 
 require_once __DIR__ . '/../Models/Barbearias.php';
-require_once __DIR__ . '/../Models/Equipe.php';
+require_once __DIR__ . '/../Models/Usuarios.php';
 
 class Usuarios {
 
     public function index(){
-        require __DIR__ . '/../Views/Usuarios/index.php';
+
+        require __DIR__ .
+        '/../Views/Usuarios/index.php';
+
     }
 
     public function new(){
-        require __DIR__ . '/../Views/Usuarios/new.php';
+
+        require __DIR__ .
+        '/../Views/Usuarios/new.php';
+
     }
 
     public function save(){
 
-        $dono = $_POST['dono'] ?? null;
-        $barbearia = $_POST['barbearia'] ?? null;
-        $email = $_POST['email'] ?? null;
-        $cpf = $_POST['cpf'] ?? null;
-        $fone = $_POST['fone'] ?? null;
-        $senha = $_POST['senha'] ?? null;
+        $dono =
+        $_POST['dono']
+        ?? null;
 
-        if(!$dono || !$barbearia || !$email || !$cpf || !$fone || !$senha){
-            echo "Preencha todos os campos";
+        $barbearia =
+        $_POST['barbearia']
+        ?? null;
+
+        $email =
+        $_POST['email']
+        ?? null;
+
+        $cpf =
+        $_POST['cpf']
+        ?? null;
+
+        $fone =
+        $_POST['fone']
+        ?? null;
+
+        $senha =
+        $_POST['senha']
+        ?? null;
+
+        if(
+
+            !$dono
+            ||
+            !$barbearia
+            ||
+            !$email
+            ||
+            !$cpf
+            ||
+            !$fone
+            ||
+            !$senha
+
+        ){
+
+            echo
+            "Preencha todos os campos";
+
             return;
+
         }
 
-        $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
+        $senhaHash =
+        password_hash(
+            $senha,
+            PASSWORD_DEFAULT
+        );
 
-        // 1. CRIA BARBEARIA
-        $barbearia_id = Barbearias::save(
+        // cria barbearia
+
+        $barbearia_id =
+        Barbearias::save(
+
             $barbearia,
+
             $email,
+
             $cpf,
+
             $fone
+
         );
 
-        // 2. CRIA DONO NA EQUIPE
-        Equipe::save(
+        // cria dono
+
+        Usuarios::save(
+
             $barbearia_id,
+
             $dono,
+
             $email,
+
+            $cpf,
+
+            $fone,
+
             $senhaHash,
+
             'dono',
+
+            1,
+
             1
+
         );
 
-        header("Location: " . base_url('login'));
+        header(
+            "Location: "
+            .
+            base_url(
+                'login'
+            )
+        );
+
         exit;
+
     }
+
 }
