b659cd35970e36b356b92798894f28a828752a53
Gabriel <aifgoiano@gmail.com>
2026-06-09 19:50:15 -0300

    ideias

--- www/Views/templates/nav_barbeiro.php
+++ www/Views/templates/nav_barbeiro.php
@@ -27,13 +27,6 @@
 
             <li class="nav-item">
                 <a class="nav-link" href="#">
-                    <i class="bi bi-people"></i>
-                    Minha Equipe
-                </a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="#">
                     <i class="bi bi-scissors"></i>
                     Serviços
                 </a>
