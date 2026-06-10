187a0c768ba3efef8e8a5d84a1834374180c7d66
Gabriel <aifgoiano@gmail.com>
2026-06-09 21:02:12 -0300

    Adicionando verificação em dono

--- www/Views/dono/equipe.php
+++ www/Views/dono/equipe.php
@@ -1,3 +1,19 @@
+<?php 
+if(isset($_SESSION['usuario_logado'])){
+    if($_SESSION['usuario_logado']->cargo == 'dono'){
+?>
+
 <h1>Minha Equipe</h1>
 
 <a href="#">Adicionar Barbeiro</a>
+
+<?php
+    }else{
+        $msg = "Sem permissão de acesso!";
+        redirectPage(base_url('login'));
+    }
+}else{
+    $msg = "Sem permissão de acesso!";
+        redirectPage(base_url('login'));
+
+}
--- www/Views/dono/servicos.php
+++ www/Views/dono/servicos.php
@@ -1,1 +1,17 @@
+<?php 
+if(isset($_SESSION['usuario_logado'])){
+    if($_SESSION['usuario_logado']->cargo == 'dono'){
+?>
+
 <h1>Serviços</h1>
+
+<?php
+    }else{
+        $msg = "Sem permissão de acesso!";
+        redirectPage(base_url('login'));
+    }
+}else{
+    $msg = "Sem permissão de acesso!";
+        redirectPage(base_url('login'));
+
+}
